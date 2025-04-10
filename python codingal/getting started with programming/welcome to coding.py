e9835e7d0967a79c890eb5010c2456afaeded633
coder727 <prathik071212@gmail.com>
2025-04-10 19:45:41 +0700

    Add files via upload

---
+++ python codingal/getting started with programming/welcome to coding.py
@@ -0,0 +1,10 @@
+#Print a Sentence
+print("Welcome to the world of programming")
+#print a number
+print(4)
+#Next Line after print
+print("Hello Python \n")
+#Pass multiple arguements for printing
+print("hello",5)
+#End arguement of Print Statement
+print("welcome to ",end="*")
---
+++ python codingal/print.py
@@ -0,0 +1,8 @@
+#printing a greeting message
+print ("Hello world")
+#printing my name
+print ("My name is prathik")
+#printing a funfact about me
+print("I am good at maths")
+#printing a simple calculation
+print("The answer to 4 + 7 is",4 +7)
