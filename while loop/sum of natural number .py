650fc08003b339b68cee76ec89efbe54ebe42277
coder727 <prathik071212@gmail.com>
2025-05-01 19:43:35 +0700

    while loop

---
+++ while loop/sum of natural number .py
@@ -0,0 +1,10 @@
+#input the value the terms 
+n = int(input("enter the value of terms:"))
+print()
+sum =0 #initialize sum value
+i = 1 # initialize i value
+while i <= n :#loop will run from 1 to n
+ sum = sum +i
+ i= i+1
+
+print("sum=", sum) 
